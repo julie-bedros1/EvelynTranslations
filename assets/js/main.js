6e945d8369a27873a4fedfeabf083e23a5774e65
marine <marine.krikorian.2@gmail.com>
2024-10-01 13:17:34 +0300

    Homepage Design and Implementation

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+
+  /**
+   * Init swiper tabs sliders
+   */
+  function initSwiperTabs() {
+    document
+      .querySelectorAll(".init-swiper-tabs")
+      .forEach(function(swiperElement) {
+        let config = JSON.parse(
+          swiperElement.querySelector(".swiper-config").innerHTML.trim()
+        );
+
+        const dotsContainer = swiperElement
+          .closest("section")
+          .querySelector(".js-custom-dots");
+        if (!dotsContainer) return;
+
+        const customDots = dotsContainer.querySelectorAll("a");
+
+        // Remove the default pagination setting
+        delete config.pagination;
+
+        const swiperInstance = new Swiper(swiperElement, config);
+
+        swiperInstance.on("slideChange", function() {
+          updateSwiperTabsPagination(swiperInstance, customDots);
+        });
+
+        customDots.forEach((dot, index) => {
+          dot.addEventListener("click", function(e) {
+            e.preventDefault();
+            swiperInstance.slideToLoop(index);
+            updateSwiperTabsPagination(swiperInstance, customDots);
+          });
+        });
+
+        updateSwiperTabsPagination(swiperInstance, customDots);
+      });
+  }
+
+  function updateSwiperTabsPagination(swiperInstance, customDots) {
+    const activeIndex = swiperInstance.realIndex;
+    customDots.forEach((dot, index) => {
+      if (index === activeIndex) {
+        dot.classList.add("active");
+      } else {
+        dot.classList.remove("active");
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiperTabs);
